58960740c08345397c214eb781a67c586c3c19cf
ram5353 <ramkiran5353@gmail.com>
2020-10-21 15:23:35 +0530

    SubSet sum problem top down approach

--- src/com/company/KnapSack/SubSetSum.java
+++ src/com/company/KnapSack/SubSetSum.java
@@ -3,11 +3,11 @@ package com.company.KnapSack;
 public class SubSetSum {
 
     public static void main(String[] args) {
-        int[] values = {3, 34, 4, 12, 13, 6};
+        int[] values = {3, 34, 4, 12, 13, 5};
         SubSet obj = new SubSet();
         System.out.println(obj.findSubSetSumRecursive(values, 9, values.length));
+        System.out.println(obj.findSubsetSumTopDown(values, values.length, 9));
     }
-
 }
 
 class SubSet {
@@ -23,5 +23,36 @@ class SubSet {
         } else {
             return findSubSetSumRecursive(values, sum, n-1);
         }
+    }
+
+    public boolean findSubsetSumTopDown(int set[],
+                               int n, int sum)
+    {
+        // The value of subset[i][j] will be
+        // true if there is a subset of
+        // set[0..j-1] with sum equal to i
+        boolean subset[][] = new boolean[sum + 1][n + 1];
+
+        // If sum is 0, then answer is true
+        for (int i = 0; i <= n; i++)
+            subset[0][i] = true;
+
+        // If sum is not 0 and set is empty,
+        // then answer is false
+        for (int i = 1; i <= sum; i++)
+            subset[i][0] = false;
+
+        // Fill the subset table in botton
+        // up manner
+        for (int i = 1; i <= sum; i++) {
+            for (int j = 1; j <= n; j++) {
+                subset[i][j] = subset[i][j - 1];
+                if (i >= set[j - 1])
+                    subset[i][j] = subset[i][j]
+                            || subset[i - set[j - 1]][j - 1];
+            }
+        }
+
+        return subset[sum][n];
     }
 }
